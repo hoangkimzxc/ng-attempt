34f9325ffbb5af3cd59637b892a9941af90533fb
hoangnk <hoangkimzxc@gmail.com>
2022-05-29 20:38:38 +0700

    6_Property Binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,41 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `<h1>Welcome to Codersx</h1>
-    <app-hello></app-hello>
-    <app-hi></app-hi>
-    <app-howdy></app-howdy>`,
+  template: `
+    <h1 [ngClass]="{ 'with-border': withBorder }">
+      {{ title }}
+    </h1>
+    <h1 [class.with-border]="withBorder">
+      {{ title }}
+    </h1>
+    <h1 [ngStyle]="styleObj">
+      {{ title }}
+    </h1>
+    <h1 [ngStyle]="{ color: textColor, background: backgroundColor }">
+      {{ title }}
+    </h1>
+    <h1
+      [textContent]="title"
+      [style.color]="textColor"
+      [style.background]="backgroundColor"
+    ></h1>
+    <img src="{{ imageSrc }}" />
+    <img [src]="imageSrc" [class] [attr.id] />
+  `,
+
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'ng-coders-tokyo';
+  // Access Modify
+  title: string = 'Coders.Tokyo Updated';
+  imageSrc = 'https://picsum.photos/200';
+  textColor = 'tomato';
+  backgroundColor = 'black';
+  withBorder = true;
+
+  styleObj = { color: this.textColor, background: this.backgroundColor };
 }
+
+// DataBinding
+// 1. PropertyBinding
+// 2. EventBinding
